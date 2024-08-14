d690e9c107579f2eb854c723fa6ec8a96e04e2f9
philip-hub <philipstudentemail@gmail.com>
2024-08-14 14:52:39 -0400

    here is the test.js

--- src/pages/test3.js
+++ src/pages/test3.js
@@ -729,7 +729,7 @@ export default function Home() {
                     {Object.keys(cloneMapping).map((arm, index) => (
                         <div key={index} className={styles.chromosomeArm}>
                             <label htmlFor={`chromosome-arm-${index}`}>
-                                {arm.toUpperCase().replace('CHR', '')} S:{s0Mapping[arm] || 'N/A'}
+                                {arm.toUpperCase().replace('CHR', '')} S0 : {s0Mapping[arm] || 'N/A'}
                             </label>
                             <input
                                 type="checkbox"
